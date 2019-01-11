20d879c294ed6f34fb8b51899dbd41121faf4c2f
Alpha2051 <brahim.guedich@gmail.com>
2019-01-11 00:43:07 +0000

    level one design

--- Dancing_Line/Assets/Scripts/cubeMovement.cs
+++ Dancing_Line/Assets/Scripts/cubeMovement.cs
@@ -9,7 +9,10 @@ public class cubeMovement : MonoBehaviour
     private Vector3 pos;
     public Material mat;
 
-    private int lCount =1 ;
+    private int lCount = 1 ;
+    public bool onGround = true;
+    public float distFromGround = 0.6f;
+    public bool isAlive = true;
 
     // Start is called before the first frame update
     void Start()
@@ -20,24 +23,76 @@ public class cubeMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        pos = Actor.transform.position;
-        Actor.transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        GameObject Actor2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Actor2.transform.position = pos;
-        Actor2.GetComponent<MeshRenderer>().material = mat;
-        Actor2.GetComponent<BoxCollider>().isTrigger = true;
-        if (Input.GetMouseButtonDown(0) )
-        {
-            if(lCount % 2 !=0)
-            {
-                Actor.transform.eulerAngles = new Vector3(0, 90, 0);
-                lCount++;
-            }
-            else
-            {
-                Actor.transform.eulerAngles = new Vector3(0, 0, 0);
-                lCount++;
+        if (isAlive) { 
+            onGround = isGrounded(); 
+            pos = Actor.transform.position;
+            Actor.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            if(onGround == true) { 
+                GameObject Actor2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                Actor2.transform.position = pos;
+                Actor2.GetComponent<MeshRenderer>().material = mat;
+                Actor2.GetComponent<BoxCollider>().isTrigger = true;
+                if (Input.GetMouseButtonDown(0) )
+                {
+                    if(lCount % 2 !=0)
+                    {
+                        Actor.transform.eulerAngles = new Vector3(0, 90, 0);
+                        lCount++;
+                    }
+                    else
+                    {
+                        Actor.transform.eulerAngles = new Vector3(0, 0, 0);
+                        lCount++;
+                    }
+                }
             }
         }
     }
+
+    public bool isGrounded()
+    {
+        return Physics.Raycast(Actor.transform.position, Vector3.down, distFromGround);
+    }
+
+    private void OnCollisionEnter( Collision collision)
+    {
+        if(collision.gameObject.tag == "obstacle")
+        {
+            isAlive = false;
+            GameObject Prime = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            GameObject Prime1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            GameObject Prime2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            GameObject Prime3 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            GameObject Prime4 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            GameObject Prime5 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            GameObject Prime6 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+            Prime.transform.position = pos;
+            Prime1.transform.position = pos;
+            Prime2.transform.position = pos;
+            Prime3.transform.position = pos;
+            Prime4.transform.position = pos;
+            Prime5.transform.position = pos;
+            Prime6.transform.position = pos;
+
+            Prime.AddComponent<Rigidbody>().velocity = Random.onUnitSphere * 50;
+            Prime1.AddComponent<Rigidbody>().velocity = Random.onUnitSphere * 100;
+            Prime2.AddComponent<Rigidbody>().velocity = Random.onUnitSphere * 10;
+            Prime3.AddComponent<Rigidbody>().velocity = Random.onUnitSphere * 20;
+            Prime4.AddComponent<Rigidbody>().velocity = Random.onUnitSphere * 60;
+            Prime5.AddComponent<Rigidbody>().velocity = Random.onUnitSphere * 5;
+            Prime6.AddComponent<Rigidbody>().velocity = Random.onUnitSphere * 1;
+
+            Prime.GetComponent<MeshRenderer>().material = mat;
+            Prime1.GetComponent<MeshRenderer>().material = mat;
+            Prime2.GetComponent<MeshRenderer>().material = mat;
+            Prime3.GetComponent<MeshRenderer>().material = mat;
+            Prime4.GetComponent<MeshRenderer>().material = mat;
+            Prime5.GetComponent<MeshRenderer>().material = mat;
+            Prime6.GetComponent<MeshRenderer>().material = mat;
+
+        }
+    }
 }
+
+
